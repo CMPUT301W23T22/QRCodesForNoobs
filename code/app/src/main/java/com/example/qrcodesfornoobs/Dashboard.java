b8ab777d820ba8e02270f2a1165846eb0606fa58
thomas huynh <thomashuynh1311@gmail.com>
2023-02-19 13:38:48 -0700

    added nav bar for main screen

--- code/app/src/main/java/com/example/qrcodesfornoobs/Dashboard.java
+++ code/app/src/main/java/com/example/qrcodesfornoobs/Dashboard.java
@@ -2,26 +2,25 @@ package com.example.qrcodesfornoobs;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.qrcodesfornoobs.databinding.DashboardBinding;
+import com.google.android.material.navigation.NavigationBarView;
 import com.smarteist.autoimageslider.SliderView;
 
 import java.util.ArrayList;
 
 public class Dashboard extends AppCompatActivity {
-    ImageButton profileImageButton;
-    ImageButton settingImageButton;
-    SliderView codeSliderView;
-    Button searchButton;
-    Button mapButton;
-    Button leaderboardButton;
-    Button cameraButton;
-    Button homeButton;
+
+    DashboardBinding binding; // xml binding class (to avoid using R.id.)
+
     private Intent profileIntent;
     private Intent settingsIntent;
     private Intent searchIntent;
@@ -33,7 +32,9 @@ public class Dashboard extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.dashboard);
+        binding = DashboardBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+//        setContentView(R.layout.dashboard);
 
         profileIntent = new Intent(this, Profile.class);
         settingsIntent = new Intent(this, Settings.class);
@@ -50,71 +51,53 @@ public class Dashboard extends AppCompatActivity {
         String test_url1 = "https://www.geeksforgeeks.org/wp-content/uploads/gfg_200X200-1.png";
         String test_url2 = "https://bizzbucket.co/wp-content/uploads/2020/08/Life-in-The-Metro-Blog-Title-22.png";
 
-        codeSliderView = findViewById(R.id.dashboard_sliderView);
         ArrayList<String> codeURLs = new ArrayList<>();
         codeURLs.add(test_url1);
         codeURLs.add(test_url2);
 
         CodeSliderAdapter adapter = new CodeSliderAdapter(this, codeURLs);
-        codeSliderView.setSliderAdapter(adapter);
+        binding.dashboardSliderView.setSliderAdapter(adapter);
     }
 
 
     private void addListenerOnButtons() {
-        profileImageButton = (ImageButton) findViewById(R.id.profile_imageButton);
-        profileImageButton.setOnClickListener(new View.OnClickListener() {
+        binding.profileImageButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 startActivity(profileIntent);
             }
         });
 
-        settingImageButton = (ImageButton) findViewById(R.id.setting_imageButton);
-        settingImageButton.setOnClickListener(new View.OnClickListener() {
+        binding.settingImageButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 startActivity(settingsIntent);
             }
         });
 
-//        homeButton = (Button) findViewById(R.id.dashboard_button);
-//        homeButton.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                startActivity(dashboardIntent);
-//            }
-//        });
-//
-//        searchButton = (Button) findViewById(R.id.search_button);
-//        searchButton.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                startActivity(searchIntent);
-//            }
-//        });
-//
-//        cameraButton = (Button) findViewById(R.id.camera_button);
-//        cameraButton.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                startActivity(cameraIntent);
-//            }
-//        });
-//
-//        leaderboardButton = (Button) findViewById(R.id.leaderboard_button);
-//        leaderboardButton.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                startActivity(leaderboardIntent);
-//            }
-//        });
-//
-//        mapButton = (Button) findViewById(R.id.map_button);
-//        mapButton.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                startActivity(mapIntent);
-//            }
-//        });
+        binding.bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.home:
+                        startActivity(dashboardIntent);
+                        break;
+                    case R.id.search:
+                        startActivity(searchIntent);
+                        break;
+                    case R.id.camera:
+                        startActivity(cameraIntent);
+                        break;
+                    case R.id.leaderboard:
+                        startActivity(leaderboardIntent);
+                        break;
+                    case R.id.map:
+                        startActivity(mapIntent);
+                        break;
+
+                }
+                return true;
+            }
+        });
     }
 }
