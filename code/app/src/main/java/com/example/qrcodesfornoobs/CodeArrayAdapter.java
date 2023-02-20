4f4752940aec5c89e8a06bd2cb16771ef7abb639
sepniojr <sepniojr@ualberta.ca>
2023-02-20 14:36:23 -0700

    Added filter bar for listview

--- code/app/src/main/java/com/example/qrcodesfornoobs/CodeArrayAdapter.java
+++ code/app/src/main/java/com/example/qrcodesfornoobs/CodeArrayAdapter.java
@@ -1,2 +1,34 @@
-package com.example.qrcodesfornoobs;public class CodeArrayAdapter {
+package com.example.qrcodesfornoobs;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import java.util.ArrayList;
+
+public class CodeArrayAdapter extends ArrayAdapter<String> {
+
+    public CodeArrayAdapter(@NonNull Context context, ArrayList<String> codes) {
+        super(context, 0, codes);
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent){
+        View view;
+        if (convertView == null) {
+            view = LayoutInflater.from(getContext()).inflate(R.layout.content,
+                    parent, false);
+        } else {
+            view = convertView;
+        }
+
+        return view;
+    }
 }
--- code/app/src/main/java/com/example/qrcodesfornoobs/Profile.java
+++ code/app/src/main/java/com/example/qrcodesfornoobs/Profile.java
@@ -1,51 +1,94 @@
 package com.example.qrcodesfornoobs;
 
-import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
 import android.widget.ListView;
+import android.widget.Spinner;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.ArrayList;
+
 public class Profile extends AppCompatActivity {
     Button backButton;
-    Button toggleButton;
+    ImageButton toggleFilterButton;
+    ImageButton toggleListViewButton;
+    ImageButton toggleSortButton;
+    Spinner sortListSpinner;
     ListView listView;
+
+    LinearLayout filterBar;
     private Intent dashboardIntent;
+
+    private ArrayList<String> dataList;
+    private ArrayAdapter<String> dataAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.profile);
-        listView = findViewById(R.id.list_view);
-        dashboardIntent = new Intent(this, Dashboard.class);
 
+        dataList = new ArrayList<>();
+        dataList.add("NAME 1");
+        dataList.add("NAME 2");
+        initWidgets();
+
+        ArrayAdapter<CharSequence> spinAdapter = ArrayAdapter.createFromResource(this,
+                R.array.filter_options, android.R.layout.simple_spinner_item);
+        spinAdapter.setDropDownViewResource(android.R.layout.simple_dropdown_item_1line);
+        sortListSpinner.setAdapter(spinAdapter);
+        dataAdapter = new CodeArrayAdapter(this, dataList);
+        listView.setAdapter(dataAdapter);
+
+        dashboardIntent = new Intent(this, Dashboard.class);
 
         addListenerOnButtons();
     }
+    private void initWidgets(){
+        backButton = findViewById(R.id.back_button);
+        backButton.setBackgroundResource(R.drawable.back_arrow);
+        toggleFilterButton = findViewById(R.id.toggle_filterbar_button);
+        toggleListViewButton = findViewById(R.id.toggle_listview_button);
+        toggleSortButton = findViewById(R.id.sort_listview_button);
+        filterBar = findViewById(R.id.filterbar);
+        sortListSpinner = findViewById(R.id.sort_list_spinner);
+        listView = findViewById(R.id.list_view);
+    }
 
     private void addListenerOnButtons() {
-        backButton = (Button) findViewById(R.id.back_button);
-        toggleButton = (Button) findViewById(R.id.toggle_listview_button);
-        backButton.setBackgroundResource(R.drawable.back_arrow);
+
         backButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 startActivity(dashboardIntent);
             }
         });
-        toggleButton.setOnClickListener(new View.OnClickListener() {
+        toggleFilterButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (filterBar.getVisibility() == View.VISIBLE){
+                    filterBar.setVisibility(View.GONE);
+                } else {
+                    filterBar.setVisibility(View.VISIBLE);
+                }            }
+        });
+        toggleListViewButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 if (listView.getVisibility() == View.VISIBLE){
+                    toggleListViewButton.setImageResource(R.drawable.face_icon);
                     listView.setVisibility(View.GONE);
                 } else {
+                    toggleListViewButton.setImageResource(R.drawable.menu_icon);
                     listView.setVisibility(View.VISIBLE);
                 }
             }
         });
+
     }
 }
