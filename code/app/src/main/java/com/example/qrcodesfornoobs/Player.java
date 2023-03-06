45c1752a8675abd9da29ecd305d15831e1850d99
PolarisStarnor <polarisstarnor@gmail.com>
2023-03-05 22:13:30 -0700

    Implemented drawing names from firebase into the creature profiles The names aren't displayed yet, since I'm assuming that's for another sprint when we can use the Creature classes.

--- code/app/src/main/java/com/example/qrcodesfornoobs/Player.java
+++ code/app/src/main/java/com/example/qrcodesfornoobs/Player.java
@@ -1,2 +1,45 @@
-package com.example.qrcodesfornoobs;public class Player {
+package com.example.qrcodesfornoobs;
+
+import android.provider.ContactsContract;
+
+import java.util.ArrayList;
+
+public class Player {
+
+    private String username;
+    private String device;
+    private ContactsContract.CommonDataKinds.Email contact;
+    private ArrayList<Creature> creatures = new ArrayList<>();
+
+    public Player(String username, String device, ContactsContract.CommonDataKinds.Email contact) {
+        this.username = username;
+        this.device = device;
+        this.contact = contact;
+    }
+
+    public void addCreature(Creature creature) {
+        creatures.add(creature);
+    }
+
+    public void removeCreature(Creature creature) {
+        creatures.remove(creature);
+    }
+
+    public void removeCreature(int i) {
+        creatures.remove(i);
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getDevice(){
+        return device;
+    }
+
+    public ContactsContract.CommonDataKinds.Email getContact() {
+        return contact;
+    }
 }
+
+
--- code/app/src/main/java/com/example/qrcodesfornoobs/SignIn.java
+++ code/app/src/main/java/com/example/qrcodesfornoobs/SignIn.java
@@ -1,15 +1,33 @@
 package com.example.qrcodesfornoobs;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.provider.Settings;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
+import java.util.Objects;
 
 public class SignIn extends AppCompatActivity {
     Button signInButton;
     private Intent dashboardIntent;
+
+    ArrayList<Player> playersList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +35,29 @@ public class SignIn extends AppCompatActivity {
         setContentView(R.layout.signin);
         dashboardIntent = new Intent(this, Dashboard.class);
         addListenerOnButtons();
+
+        // Call Firebase to ensure that a potential added username/device/email is unique
+        initializeFirebase();
+    }
+
+    private boolean checkDeviceUniqueness() {
+        String deviceID = Settings.Secure.ANDROID_ID;
+        for(Player player:playersList){
+            if(Objects.equals(player.getDevice(), deviceID)){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private boolean checkUserUniqueness(String username, String email){
+        for(Player player:playersList){
+            if(Objects.equals(player.getUsername(), username) ||
+               Objects.equals(player.getContact().toString(), email)){
+                return false;
+            }
+        }
+        return true;
     }
 
     private void addListenerOnButtons() {
@@ -25,6 +66,25 @@ public class SignIn extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 startActivity(dashboardIntent);
+            }
+        });
+    }
+
+    private void initializeFirebase(){
+
+        FirebaseApp.initializeApp(this);
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        final CollectionReference playerReference = db.collection("Players");
+        playerReference.addSnapshotListener((value, error) -> {
+
+            playersList = new ArrayList<>();
+
+            for (QueryDocumentSnapshot doc: value){
+                String username = (String) doc.get("Username");
+                String device = (String) doc.get("Device");
+                ContactsContract.CommonDataKinds.Email contact =
+                        (ContactsContract.CommonDataKinds.Email) doc.get("Contact");
+                playersList.add(new Player(username, device, contact));
             }
         });
     }
