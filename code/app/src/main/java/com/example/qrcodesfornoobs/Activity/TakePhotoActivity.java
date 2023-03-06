15b2e5e6bd1ef6a467b0b92675945c69f926f6a5
thomas huynh <thomashuynh1311@gmail.com>
2023-03-06 01:41:25 -0700

    upload image to db

--- code/app/src/main/java/com/example/qrcodesfornoobs/Activity/TakePhotoActivity.java
+++ code/app/src/main/java/com/example/qrcodesfornoobs/Activity/TakePhotoActivity.java
@@ -11,8 +11,11 @@ import android.app.Activity;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
 import android.location.Location;
 import android.media.Image;
+import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.util.Log;
@@ -20,16 +23,16 @@ import android.view.View;
 import android.widget.Toast;
 
 import com.example.qrcodesfornoobs.Creature;
-import com.example.qrcodesfornoobs.R;
-import com.example.qrcodesfornoobs.databinding.ActivityMainBinding;
 import com.example.qrcodesfornoobs.databinding.ActivityTakePhotoBinding;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
-import java.security.NoSuchAlgorithmException;
-import java.util.HashMap;
-import java.util.Map;
+import java.io.ByteArrayOutputStream;
+import java.util.concurrent.CompletableFuture;
 
 public class TakePhotoActivity extends AppCompatActivity {
     private static final int CAMERA_REQUEST = 666;
@@ -58,47 +61,84 @@ public class TakePhotoActivity extends AppCompatActivity {
                 // TODO: upload code, photo (of qr & place), location (if selected) to db
                 String scannedCode = getIntent().getExtras().getString("code");
 
-                try {
-                    Image isPhotoCreature = null;
-                    Image isPhotoLocation = null;
-                    Location isLocation = null; //setting this in part4
-                    //setting location, photo, photoLocation to null
-                    if (binding.saveImageCheckBox.isChecked()) {
-                        // set local isPhotoCreature to the photo
-                    }
-                    if (binding.saveLocationCheckBox.isChecked()) {
-                        // set local isPhotoLocation to the photo
-                    }
-
-                    Creature creature = new Creature(scannedCode,isLocation, isPhotoCreature, isPhotoLocation);
-                    //preparing the data to send to db
-                    // initializing the db & entry to add to it at the end of the process
-                    FirebaseFirestore db = FirebaseFirestore.getInstance();
-                    db.collection("Creatures").document(creature.getHash())
-                            .set(creature)
-                            .addOnSuccessListener(new OnSuccessListener<Void>() {
-                                @Override
-                                public void onSuccess(Void aVoid) {
-                                    Log.d(TAG, "DocumentSnapshot successfully written!");
-                                    Toast.makeText(getBaseContext(), "Code added successfully", Toast.LENGTH_SHORT).show();
-                                }
-                            })
-                            .addOnFailureListener(new OnFailureListener() {
-                                @Override
-                                public void onFailure(@NonNull Exception e) {
-                                    Log.w(TAG, "Error writing document", e);
-                                }
-                            });
-                    Toast.makeText(getBaseContext(), "location will be saved in Project part 4", Toast.LENGTH_SHORT).show();
-
-                } catch (NoSuchAlgorithmException e) {
-                    e.printStackTrace();
-                    Toast.makeText(getBaseContext(), "scanned code can't be hashed...", Toast.LENGTH_SHORT).show();
+                Image photoCreature = null;
+                Location location = null; //setting this in part4
+                if (binding.saveLocationCheckBox.isChecked()) {
+                    // set local isPhotoLocation to the photo
+                }
+                if (binding.saveImageCheckBox.isChecked()) {
+                    uploadCurrentImage().thenAccept(photoLocationUrl -> {
+                        Creature creature = new Creature(scannedCode, location, photoCreature, photoLocationUrl);
+                        uploadToDatabase(creature);
+                    }).exceptionally(e -> {
+                        Toast.makeText(getBaseContext(), "Failed to upload image!", Toast.LENGTH_SHORT).show();
+                        return null;
+                    });
+                } else {
+                    Creature creature = new Creature(scannedCode, location, photoCreature, null);
+                    uploadToDatabase(creature);
                 }
                 finish();
             }
         });
         openCamera();
+    }
+
+    private void uploadToDatabase(Creature creature) {
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        db.collection("Creatures").document(creature.getHash())
+                .set(creature)
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        Toast.makeText(getBaseContext(), "Code added successfully!", Toast.LENGTH_SHORT).show();
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(getBaseContext(), "Failed to add code!", Toast.LENGTH_SHORT).show();
+                        Log.w(TAG, "Error writing document", e);
+                    }
+                });
+    }
+
+    private CompletableFuture<Uri> uploadCurrentImage() {
+        CompletableFuture<Uri> future = new CompletableFuture<>();
+        StorageReference storageReference = FirebaseStorage.getInstance().getReference("code_location/test");
+        Drawable drawable = binding.imageView.getDrawable();
+        if (drawable == null) {
+            future.complete(null);
+            return future;
+        }
+        Bitmap bitmap = ((BitmapDrawable) drawable).getBitmap();
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, baos);
+        byte[] data = baos.toByteArray();
+
+        UploadTask uploadTask = storageReference.putBytes(data);
+        uploadTask.addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                future.completeExceptionally(e);
+            }
+        }).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                storageReference.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                    @Override
+                    public void onSuccess(Uri uri) {
+                        future.complete(uri);
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        future.completeExceptionally(e);
+                    }
+                });
+            }
+        });
+        return future;
     }
 
     private void openCamera() {
@@ -113,7 +153,6 @@ public class TakePhotoActivity extends AppCompatActivity {
             if (resultCode == Activity.RESULT_OK) {
                 Bitmap bitmap = (Bitmap) data.getExtras().get("data");
                 binding.imageView.setImageBitmap(bitmap);
-                Toast.makeText(getBaseContext(), "Take photo successfully", Toast.LENGTH_SHORT).show();
             }
         }
     }
