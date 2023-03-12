e64436e8e4ca978f7278ca45c78de6312dd904ef
thomas huynh <thomashuynh1311@gmail.com>
2023-03-12 15:44:15 -0600

    US 08.01.01 Intent Testing

--- code/app/src/androidTest/java/com/example/qrcodesfornoobs/ThomasTest.java
+++ code/app/src/androidTest/java/com/example/qrcodesfornoobs/ThomasTest.java
@@ -1,6 +1,7 @@
 package com.example.qrcodesfornoobs;
 
 import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
@@ -10,12 +11,18 @@ import android.graphics.drawable.BitmapDrawable;
 import android.view.View;
 import android.widget.ImageView;
 
+import androidx.annotation.NonNull;
 import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.rule.ActivityTestRule;
 
 import com.example.qrcodesfornoobs.Activity.TakePhotoActivity;
 import com.example.qrcodesfornoobs.Models.Creature;
 import com.example.qrcodesfornoobs.Models.Player;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.robotium.solo.Solo;
 
@@ -24,11 +31,13 @@ import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 
+import java.util.concurrent.CountDownLatch;
+
 
 public class ThomasTest {
     private Solo solo;
-    private static String MOCK_USERNAME = "_ThomasTest_";
-    private static String MOCK_CODE = "###Thomas###";
+    private final static String MOCK_USERNAME = "_ThomasTest_";
+    private final static String MOCK_CODE = "###Thomas###";
     private FirebaseFirestore db;
     @Rule
     public ActivityTestRule<TakePhotoActivity> rule = new ActivityTestRule<>(TakePhotoActivity.class,true,false);
@@ -42,20 +51,12 @@ public class ThomasTest {
         db = FirebaseFirestore.getInstance();
         Player mockPlayer = new Player(MOCK_USERNAME, "123321456654");
         db.collection("Players").document(MOCK_USERNAME).set(mockPlayer);
-        // Get a reference to the SharedPreferences object of the launched activity
-//        SharedPreferences sharedPreferences = rule.getActivity().getSharedPreferences(SignInActivity.CACHE_NAME, Context.MODE_PRIVATE);
-//        SharedPreferences.Editor editor = sharedPreferences.edit();
-//        editor.remove("username");
-//        editor.apply();
         Player.LOCAL_USERNAME = MOCK_USERNAME;
     }
     @Test
-    public void addNewQRCodeToAccountTest() throws InterruptedException { // US 01.01.01
-        // Create a new intent with the target activity class
+    public void addNewQRCodeToAccountTest() { // US 01.01.01
         Intent intent = new Intent(InstrumentationRegistry.getInstrumentation().getTargetContext(), TakePhotoActivity.class);
-        // Add the String extra to the intent
         intent.putExtra("code", MOCK_CODE);
-        // Set the intent on the rule object
         rule.launchActivity(intent);
 
         solo.waitForActivity(TakePhotoActivity.class);
@@ -67,9 +68,7 @@ public class ThomasTest {
     @Test
     public void ableToRecordPhotoLocationTest() { // part of US 02.01.01
         Intent intent = new Intent(InstrumentationRegistry.getInstrumentation().getTargetContext(), TakePhotoActivity.class);
-        // Add the String extra to the intent
         intent.putExtra("code", MOCK_CODE);
-        // Set the intent on the rule object
         rule.launchActivity(intent);
         solo.waitForActivity(TakePhotoActivity.class);
         View cameraButton = solo.getView(R.id.cameraButton);
@@ -80,11 +79,9 @@ public class ThomasTest {
     }
 
     @Test
-    public void uniqueVisualRepresentationCode() { // US 02.06.01
+    public void uniqueVisualRepresentationCodeTest() { // US 02.06.01
         Intent intent1 = new Intent(InstrumentationRegistry.getInstrumentation().getTargetContext(), TakePhotoActivity.class);
-        // Add the String extra to the intent
         intent1.putExtra("code", MOCK_CODE + System.currentTimeMillis());
-        // Set the intent on the rule object
         rule.launchActivity(intent1);
         solo.waitForActivity(TakePhotoActivity.class);
         ImageView imageView1 = solo.getImage(0);
@@ -103,6 +100,36 @@ public class ThomasTest {
         assertFalse(bitmap1.sameAs(bitmap2));
     }
 
+    @Test
+    public void doNotRecordActualCodeTest() { // US 08.01.01
+        CountDownLatch latch = new CountDownLatch(1);
+        Intent intent1 = new Intent(InstrumentationRegistry.getInstrumentation().getTargetContext(), TakePhotoActivity.class);
+        intent1.putExtra("code", MOCK_CODE);
+        rule.launchActivity(intent1);
+        solo.sleep(500);
+        solo.waitForActivity(TakePhotoActivity.class);
+        solo.clickOnView(solo.getView(R.id.confirm_button));
+        solo.sleep(2000); // give some time to ensure db is updated
+        DocumentReference creatureRef =  db.collection("Creatures").document(new Creature(MOCK_CODE, null).getHash());
+        creatureRef.get().addOnCompleteListener(task -> {
+            if (task.isSuccessful()) {
+                DocumentSnapshot doc = task.getResult();
+                if (doc.exists()) {
+                    String hashValue = doc.getString("hash");
+                    assertNotEquals(MOCK_CODE, hashValue);
+                }
+            }
+            latch.countDown();
+        });
+        try {
+            latch.await();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+
     @After
     public void tearDown() {
         db.collection("Players").document(MOCK_USERNAME).delete();
@@ -110,68 +137,4 @@ public class ThomasTest {
         solo.finishOpenedActivities();
     }
 
-//    /**
-//     * Signs into the app as a new user, entry will then be deleted at the end of the test.
-//     * @throws Exception
-//     */
-//    @Test
-//    public void SuccessfulSignIn() throws Exception{
-//        Activity activity = rule.getActivity();
-//        solo.assertCurrentActivity("Wrong Activity",SignInActivity.class);
-//        solo.enterText((EditText) solo.getView(R.id.username_EditText),"TestReynel");
-//        solo.clickOnView(solo.getView(R.id.sign_in_button));
-//        solo.assertCurrentActivity("Wrong Activity", MainActivity.class);
-//        //deleting the added entry
-//        FirebaseFirestore db = FirebaseFirestore.getInstance();
-//        db.collection("Players")
-//                .document("TestReynel")
-//                .delete()
-//                .addOnSuccessListener(new OnSuccessListener<Void>() {
-//                    @Override
-//                    public void onSuccess(Void aVoid) {
-//                        Log.d(TAG, "DocumentSnapshot successfully deleted!");
-//                    }
-//                })
-//                .addOnFailureListener(new OnFailureListener() {
-//                    @Override
-//                    public void onFailure(@NonNull Exception e) {
-//                        Log.w(TAG, "Error deleting document", e);
-//                    }
-//                });
-//    }
-//    /**
-//     * Signs into the app as a existing user with a different device, should remain on signin page.
-//     * @throws Exception
-//     */
-//    @Test
-//    public void UnsuccessfulSignIn() throws Exception{
-//        Activity activity = rule.getActivity();
-//        solo.assertCurrentActivity("Wrong Activity",SignInActivity.class);
-//        EditText editText = (EditText) solo.getView(R.id.username_EditText);
-//        solo.enterText(editText,"differentDevice");
-//        solo.clickOnView(solo.getView(R.id.sign_in_button));
-//        solo.waitForText("Username Already Exists!");
-//        String error1 = editText.getError().toString();
-//        assertEquals("Username Already Exists!", error1);
-//        solo.assertCurrentActivity("Wrong Activity", SignInActivity.class);
-//        solo.clearEditText(editText);
-//        solo.enterText(editText,"");
-//        solo.clickOnView(solo.getView(R.id.sign_in_button));
-//        solo.clickOnView(solo.getView(R.id.sign_in_button));
-//        solo.waitForText("Provide a Username!");
-//        String error2 = editText.getError().toString();
-//        assertEquals("Provide a Username!", error2);
-//    }
-//    /**
-//     * Clears username cache after every test
-//     */
-//    @After
-//    public void cleanup(){
-//        SharedPreferences sharedPreferences = rule.getActivity().getSharedPreferences(SignInActivity.CACHE_NAME, Context.MODE_PRIVATE);
-//        SharedPreferences.Editor editor = sharedPreferences.edit();
-//        editor.remove("username");
-//        editor.apply();
-//        System.out.println(sharedPreferences.getString("username",""));
-//    }
-//    String TAG = "IntentTesting";
 }
