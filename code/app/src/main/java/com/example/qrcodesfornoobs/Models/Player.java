eba31905225d9a7bec75c5d1d1e1882dde3adaab
PolarisStarnor <polarisstarnor@gmail.com>
2023-03-12 08:26:52 -0600

    Added comments for the player class methods, ignoring the hashCode() method

--- code/app/src/main/java/com/example/qrcodesfornoobs/Models/Player.java
+++ code/app/src/main/java/com/example/qrcodesfornoobs/Models/Player.java
@@ -3,6 +3,9 @@ package com.example.qrcodesfornoobs.Models;
 import java.util.ArrayList;
 import java.util.Objects;
 
+/**
+ * Represents a single player of the game, using that player's username and deviceID.
+ */
 public class Player {
     public static String LOCAL_USERNAME = null; // only set if user has signed in before
     //TODO: Call to db for Creatures
@@ -11,6 +14,11 @@ public class Player {
     private ArrayList<String> creatures = new ArrayList<>();
     private String contact;
 
+    /**
+     * Constructor for a player class.
+     * @param username Username's unique username. (Uniqueness is checked outside the constructor.)
+     * @param device    ID for the device the user logged in on.
+     */
     public Player(String username, String device) {
         this.username = username;
         this.device = device;
@@ -39,32 +47,70 @@ public class Player {
         return Objects.hash(username, device);
     }
 
+    /**
+     * Add a creature's hash to player's collection.
+     * @param creature Creature to be added.
+     * @see Creature
+     */
     public void addCreature(Creature creature) {
         creatures.add(creature.getHash());
     }
 
+    /**
+     * Removes target creature from Player's collection.
+     * Does nothing if the Creature's hash is not in Player's collection.
+     * @param creature Creature to be deleted.
+     */
     public void removeCreature(Creature creature) {
         creatures.remove(creature.getHash());
     }
 
+    /**
+     * Removes target creature from Player's collection.
+     * Does nothing if the Creature's hash is not in Player's collection.
+     * @param i index of the creature being removed.
+     */
     public void removeCreature(int i) {
         creatures.remove(i);
     }
 
+    /**
+     * Getter function for player's username.
+     * @return Player's username
+     */
     public String getUsername() {
         return username;
     }
 
+    /**
+     * Getter function for the player's associated device.
+     * @return DeviceID of device the player used to login.
+     */
     public String getDevice(){
         return device;
     }
 
+    /**
+     * Get the contact information for the player.
+     * @return String with contact information.
+     */
     public String getContact() {
         return contact;
     }
+
+    /**
+     * Returns the arraylist containing the hashes for all the player's Creatures.
+     * @return Arraylist of player's hashes.
+     */
     public ArrayList<String> getCreatures() {
         return creatures;
     }
+
+    /**
+     * Checks if a given Creature is in a player's collection.
+     * @param creature Creature class with a hash.
+     * @return True if player has the target Creature. False otherwise.
+     */
     public boolean containsCreature(Creature creature) {
         return creatures.contains(creature.getHash());
     }
